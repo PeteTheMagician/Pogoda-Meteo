baf5ed863a5ed8a706d45e73126f68125f663ab5
PeteTheMagician <fksdroid@gmail.com>
2015-06-27 13:54:30 +0200

    Regexp works for GPS and by id query

--- app/src/main/java/pl/dawidfiruzek/pogodameteo/FetchWeatherTask.java
+++ app/src/main/java/pl/dawidfiruzek/pogodameteo/FetchWeatherTask.java
@@ -23,19 +23,17 @@ public class FetchWeatherTask extends AsyncTask<Void, Void, Bitmap> {
     public FetchWeatherTask(ImageView imageView){
         mImage = imageView;
     }
+//
+//    http://new.meteo.pl/um/php/mgram_search.php?NALL=50.25&EALL=19&lang=pl"
 //    http://new.meteo.pl/um/php/meteorogram_id_um.php?ntype=0n&id=462
 //    http://new.meteo.pl/um/metco/mgram_pict.php?ntype=0u&fdate=2015061812&row=466&col=232&lang=pl
-
+//
+//    http://new.meteo.pl/php/mgram_search.php?NALL=50.25&EALL=19&lang=pl
 //    http://new.meteo.pl/php/meteorogram_id_coamps.php?ntype=2n&id=462
 //    http://new.meteo.pl/metco/mgram_pict.php?ntype=2n&fdate=2015061812&row=151&col=90&lang=pl
     @Override
     protected Bitmap doInBackground(Void... params) {
         Bitmap weatherImage = null;
-        String ntype = null;
-        String fdate = null;
-        String row = null;
-        String col = null;
-        String lang = null;
 
         Uri.Builder builder = new Uri.Builder();
         //TODO czytane z bazy danych dla typu pogody UM/COAMPS
@@ -50,6 +48,8 @@ public class FetchWeatherTask extends AsyncTask<Void, Void, Bitmap> {
         try{
             URL meteoUrl = new URL(builder.toString());
             Log.v(MainActivity.TAG, meteoUrl.toString());
+
+            String testUrl = "http://new.meteo.pl/php/meteorogram_id_coamps.php?ntype=2n&id=462";
 //
 //            URLConnection urlConnection = meteoUrl.openConnection();
 //            urlConnection.connect();
@@ -59,10 +59,10 @@ public class FetchWeatherTask extends AsyncTask<Void, Void, Bitmap> {
 //            bis.close();
 //            is.close();
             //TODO URL build on id of the city or GPS position
-            org.jsoup.nodes.Document doc = Jsoup.connect(meteoUrl.toString()).timeout(10000).get();
-            Element script = doc.select("script").first();
+            org.jsoup.nodes.Document doc = Jsoup.connect(testUrl).timeout(10000).get();
+            Element script = doc.select("script").last();
             //getting variables values that are needed to get meteorogram
-            Pattern pattern = Pattern.compile("(?is)var\\s??fcstdate\\s??=\\s??\"(.+?)\\n");
+            Pattern pattern = Pattern.compile("(?s)var\\s??(.+?);var\\s??(.+?)ntype(.+?);\\n");
             Matcher m = pattern.matcher(script.html());
 
             String meteorogramParams = null;
@@ -70,6 +70,10 @@ public class FetchWeatherTask extends AsyncTask<Void, Void, Bitmap> {
             while(m.find()) {
                 meteorogramParams = m.group();
                 Log.v(MainActivity.TAG, meteorogramParams);
+//                String[] separatedParams = meteorogramParams.split("var ");
+//                for(String s : separatedParams) {
+//                    Log.v(MainActivity.TAG, s);
+//                }
             }
 
             if(meteorogramParams != null){
