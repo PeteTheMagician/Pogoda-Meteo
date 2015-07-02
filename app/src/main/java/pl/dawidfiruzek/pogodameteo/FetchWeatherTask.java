0260fba4b2cc38068e3ce8468e5cf68fcc809f3f
PeteTheMagician <fksdroid@gmail.com>
2015-07-03 00:48:33 +0200

    Coamps and UM model loading works Language changing works for loading models

--- app/src/main/java/pl/dawidfiruzek/pogodameteo/FetchWeatherTask.java
+++ app/src/main/java/pl/dawidfiruzek/pogodameteo/FetchWeatherTask.java
@@ -40,33 +40,53 @@ public class FetchWeatherTask extends AsyncTask<Void, Void, Bitmap> {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(mContext);
         String gridModel = sharedPreferences.getString("grid_preference", "um");
         String language = sharedPreferences.getString("language_preference", "pl");
-//
-//    http://new.meteo.pl/um/php/mgram_search.php?NALL=50.25&EALL=19&lang=pl"
+        String updateMethod = sharedPreferences.getString("update_preference", "GPS");
+        String defaultCity = sharedPreferences.getString("city_preference", "462");
+//    http://new.meteo.pl/um/php/mgram_search.php?NALL=50.25&EALL=19&lang
 //    http://new.meteo.pl/um/php/meteorogram_id_um.php?ntype=0n&id=462
 //    http://new.meteo.pl/um/metco/mgram_pict.php
 //
 //    http://new.meteo.pl/php/mgram_search.php?NALL=50.25&EALL=19&lang=pl
 //    http://new.meteo.pl/php/meteorogram_id_coamps.php?ntype=2n&id=462
 //    http://new.meteo.pl/metco/mgram_pict.php
-//        if()
+
         Uri.Builder builder = new Uri.Builder();
-        //TODO czytane z bazy danych dla typu pogody UM/COAMPS
-        builder.scheme("http")
-                .authority("new.meteo.pl")
-                .appendPath("um")
-                .appendPath("php")
-                .appendPath("meteorogram_id_um.php")
-                .appendQueryParameter("ntype","0n")
-                .appendQueryParameter("id","462").build();
+        builder.scheme("http").authority("new.meteo.pl");
+        if(gridModel.equals("um")){
+            builder.appendPath("um");
+        }
+        builder.appendPath("php");
+
+        if(updateMethod.equals("GPS")){
+            builder
+                    .appendPath("mgram_search.php")
+                    //TODO enter actual values from GPS
+                    .appendQueryParameter("NALL", "50")
+                    .appendQueryParameter("EALL", "19").build();
+        }
+        else {
+            if (gridModel.equals("um")) {
+                builder
+                        .appendPath("meteorogram_id_um.php")
+                        .appendQueryParameter("ntype", "0u");
+            } else if (gridModel.equals("coamps")) {
+                builder
+                        .appendPath("meteorogram_id_coamps.php")
+                        .appendQueryParameter("ntype", "2n");
+            }
+            builder
+                    .appendQueryParameter("id", defaultCity)
+                    .build();
+        }
 
         try{
             URL meteoUrl = new URL(builder.toString());
             Log.d(MainActivity.TAG, meteoUrl.toString());
 
-            String testUrl = "http://new.meteo.pl/um/php/meteorogram_id_um.php?ntype=0n&id=462";
+//            String testUrl = "http://new.meteo.pl/um/php/meteorogram_id_um.php?ntype=0n&id=462";
 
             //TODO URL build on id of the city or GPS position
-            org.jsoup.nodes.Document doc = Jsoup.connect(testUrl).timeout(10000).get();
+            org.jsoup.nodes.Document doc = Jsoup.connect(meteoUrl.toString()).timeout(10000).get();
             Element script = doc.select("script").last();
             //getting variables values that are needed to get meteorogram
             Pattern pattern = Pattern.compile("(?s)var\\s??(.+?);var\\s??(.+?)ntype(.+?);\\n");
@@ -90,12 +110,25 @@ public class FetchWeatherTask extends AsyncTask<Void, Void, Bitmap> {
                         .replace(" ", "")
                         .replace(";", "");
 
+                if(language.equals("en")){
+                    meteorogramParams = meteorogramParams.replace("lang=pl", "lang=en");
+                }
+
                 Log.d(MainActivity.TAG, meteorogramParams);
             }
             else {
                 Log.e(MainActivity.TAG, "Unsuccessful image address parsing");
             }
-            String meteorogramImg = "http://new.meteo.pl/um/metco/mgram_pict.php" + meteorogramParams;
+
+            String meteorogramImg = "http://new.meteo.pl";
+            if(gridModel.equals("um")) {
+                meteorogramImg += "/um/metco/mgram_pict.php";
+            }
+            else{
+                meteorogramImg += "/metco/mgram_pict.php";
+            }
+            meteorogramImg += meteorogramParams;
+
             URL meteoImg = new URL(meteorogramImg);
             URLConnection urlConnection = meteoImg.openConnection();
             urlConnection.connect();
